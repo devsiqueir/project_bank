5cf9d7504875e71d261be192e6b6d0423b6c3648
rsiqueira011 <rsiqueira011>
2024-05-29 13:49:04 -0300

    delete

--- bank-transaction-app/backend/src/main/java/br/com/siqueiradev/backend/exceptions/BankNotFoundException.java
+++
@@ -1,8 +0,0 @@
-package br.com.siqueiradev.backend.exceptions;
-
-public class BankNotFoundException extends Throwable {
-    
-    public BankNotFoundException(String message) {
-        super(message);  // Call the superclass constructor with the message
-    }
-}
--- bank-transaction-app/backend/src/main/java/br/com/siqueiradev/backend/exceptions/ClientNotFoundException.java
+++
@@ -1,8 +0,0 @@
-package br.com.siqueiradev.backend.exceptions;
-
-public class ClientNotFoundException extends Throwable {
-    
-    public ClientNotFoundException(String message) {
-        super(message);  // Call the superclass constructor with the message
-    }
-}
